db4d0a66639de49df7d1719b9de4d6ff18ea4096
Mohitpal Singh <mohitpalsingh700@gmail.com>
2023-08-10 20:58:18 +0530

    Testing

---
+++ Backend/WebApplication1/WebApplication1/Model/APIDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Model
+{
+    public class APIDbContext:DbContext
+    {
+        public APIDbContext(DbContextOptions option):base(option)
+        {
+            
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Employee_Issue_Detail> Employee_Issue_Details { get; set; }
+        public DbSet<Loan_Card_Master> Loan_Card_Master { get; set; }
+        public DbSet<Employee_Card_Detail> Employee_Card_Details { get; set; }
+
+        public DbSet<Admin> Admin { get; set; }
+    }
+}
---
+++ Backend/WebApplication1/WebApplication1/Model/Employee_Card_Detail.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Model
+{
+    [Keyless]
+    public class Employee_Card_Detail
+    {
+
+        public Employee Employee { get; set; }
+        public Loan_Card_Master loan_Card_Master { get; set; }
+
+
+        [Required]
+        [Column(TypeName = "Date")]
+        public DateTime card_issue_date { get; set; }
+
+        
+    }
+}
