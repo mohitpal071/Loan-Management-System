778b25353d2ba12a053125d2c865eae9f9881e94
Mohitpal Singh <mohitpalsingh700@gmail.com>
2023-08-10 21:18:09 +0530

    Solved more errors

--- Backend/WebApplication1/WebApplication1/Model/APIDbContext.cs
+++ Backend/WebApplication1/WebApplication1/Model/APIDbContext.cs
@@ -2,19 +2,17 @@
 
 namespace WebApplication1.Model
 {
-    public class APIDbContext:DbContext
+    public class APIDbContext : DbContext
     {
-        public APIDbContext(DbContextOptions option):base(option)
+        public APIDbContext(DbContextOptions option) : base(option)
         {
-            
+
         }
 
         public DbSet<Employee> Employees { get; set; }
         public DbSet<Item> Items { get; set; }
         public DbSet<Employee_Issue_Detail> Employee_Issue_Details { get; set; }
         public DbSet<Loan_Card_Master> Loan_Card_Master { get; set; }
-        public DbSet<Employee_Card_Detail> Employee_Card_Details { get; set; }
-
-        public DbSet<Admin> Admin { get; set; }
+        public DbSet<Employee_Card_Detail> Employee_Card_Details { get; }
     }
 }
