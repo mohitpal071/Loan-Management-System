bdbebd140d3e6dc3344c994aaf208602a6d263b8
Mohitpal Singh <mohitpalsingh700@gmail.com>
2023-08-10 21:04:53 +0530

    Error Resolved in Program.cs

--- Backend/WebApplication1/WebApplication1/Program.cs
+++ Backend/WebApplication1/WebApplication1/Program.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:Backend1/WebApplication1/WebApplication1/Program.cs
 using Microsoft.EntityFrameworkCore;
 using WebApplication1.Model;
 
@@ -41,48 +40,4 @@ namespace WebApplication1
             app.Run();
         }
     }
-=======
-using Microsoft.EntityFrameworkCore;
-using WebApplication1.Model;
-
-namespace WebApplication1
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-
-            //Dependency Injection DbContext Class
-            builder.Services.AddDbContext<APIDbContext>(options =>
-            options.UseSqlServer(@"server=WINDOWS-BVQNF6J;database=loan;trusted_connection=true;TrustServerCertificate=True;"));
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
-        }
-    }
->>>>>>> b2ba850f458e1382e697422fff8eaa0835056a7f:Backend/loanApplication/Program.cs
 }
